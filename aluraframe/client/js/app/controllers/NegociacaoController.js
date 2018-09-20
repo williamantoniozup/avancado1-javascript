e753377860bd511011bdce17ef1690e96ffbff27
William <william.antonio@zup.com.br>
2018-09-20 16:30:51 -0300

    update

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,8 +1,8 @@
 class NegociacaoController {
 
-    constructor(){
+    constructor() {
         let $ = document.querySelector.bind(document);
-        
+
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
@@ -10,17 +10,27 @@ class NegociacaoController {
 
     adiciona(event) {
 
-        event.preventDefault(); 
+        event.preventDefault();
 
         //console.log(typeof(this._inputData.value));  uma String
 
-        let negociacao = new Negociacao(
-            this._inputData.value,
+        //'2012-12-12'
+        //let negociacao = new Negociacao(this._inputData.value); //assim nao funcionar
+        // console.log(this._inputData.value);
+        let data = new Date(...
+            this._inputData.value
+                .split('-') // arrow functions
+                .map((item, indice) => item - indice % 2)   //.map(function(item, indice){ return item - indice%2})
+        ); 
+        // console.log(data);
+
+        let negocioacao = new Negociacao(
+            data,
             this._inputQuantidade.value,
             this._inputValor.value
         );
-        
-        console.log(negociacao);
+        console.log(negocioacao);
+
         //adicionar a negociação a uma lista
     }
 }
@@ -63,5 +73,35 @@ Versao 1 -
 
       //console.log(typeof(this._inputData.value));  uma String
       Fazer um modo que está String seja convertido para um objeto date.
+
+      Há várias maneiras de construir uma data, passando parametros para seu
+      construtor.
+
+      let data = new Date(this._inputData.value.replace(/-/g, ',')); expressão regular
+
+    Podemos criar uma data desta maneira
+    let outraData = new Date('2016/05/17'); // formato ano/mês/dia
+    let outraDataOutraManeira = new Date(2016, 4, 17);
+      
+      Problemas com datas.
+
+      Como converter '2016-11-12'
+
+      para new Date(2016,10,12);
+
+      spread operator.
+      let data = new Date(...this._inputData.value.split('-'));
+      cada posição do array vai pro respectivo local no construtor do Date
+      porém do jeito que ta nao vai decrementar o mes. 
+
+         let data = new Date(...
+            this._inputData.value
+                .split('-')
+                .map(function (item, indice) {
+                    // if (indice == 1) return item - 1;
+                    // return item;
+                    return item - indice % 2;
+                })
+        );
 
 */
